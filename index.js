53fd2f7d81c1b5252fa2e87ab8f27075e801c08a
vishal <145294121+vishal2550@users.noreply.github.com>
2024-01-30 22:10:46 +0530

    this is frist commit

---
+++ index.js
@@ -0,0 +1,18 @@
+let mobile = document.getElementById("mobile")
+let iim1 = document.getElementById("iim1")
+mobile.addEventListener(
+    "click", 
+    function () {
+        document.getElementById("shoppp").style.display = "none"
+        document.querySelector(".gggg").style.display = "block"
+        
+        
+    }
+)
+
+function img2(a) {
+    iim1.src = a.src
+    
+
+}
+
